b778b85347a80bf3a8499ce937df1612814be5b5
abyss9x3 <103586132+abyss9x3@users.noreply.github.com>
2025-04-05 16:52:00 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,43 @@
-{
-  "name": "my-portfolio",
-  "private": true,
-  "description": "My Portfolio site built with Gatsby",
-  "version": "1.0.0",
-  "author": "Your Name",
-  "dependencies": {
-    "babel-plugin-styled-components": "^1.10.7",
-    "gatsby": "^3.15.0",
-    "gatsby-plugin-image": "^1.15.0",
-    "gatsby-plugin-manifest": "^3.15.0",
-    "gatsby-plugin-netlify": "^3.15.0",
-    "gatsby-plugin-react-helmet": "^4.15.0",
-    "gatsby-plugin-sharp": "^3.15.0",
-    "gatsby-plugin-sitemap": "^4.15.0",
-    "gatsby-plugin-styled-components": "^4.15.0",
-    "gatsby-source-filesystem": "^3.15.0",
-    "gatsby-transformer-sharp": "^3.15.0",
-    "gatsby-transformer-remark": "^4.15.0",
-    "gatsby-remark-images": "^5.15.0",
-    "gatsby-plugin-robots-txt": "^1.8.0",
-    "lottie-react": "^2.3.1",
-    "react": "^17.0.2",
-    "react-dom": "^17.0.2",
-    "react-helmet": "^6.1.0",
-    "styled-components": "^5.3.11"
+module.exports = {
+  siteMetadata: {
+    title: `My Portfolio`,
+    description: `This is my developer portfolio.`,
+    author: `@yourname`,
   },
-  "devDependencies": {
-    "husky": "^4.3.8"
-  },
-  "scripts": {
-    "develop": "gatsby develop",
-    "build": "gatsby build",
-    "serve": "gatsby serve",
-    "clean": "gatsby clean"
-  },
-  "keywords": [
-    "gatsby",
-    "portfolio",
-    "react"
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-remark-images`,
+      options: {
+        maxWidth: 800,
+        showCaptions: true,
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `my-portfolio`,
+        short_name: `portfolio`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+      },
+    },
   ],
-  "license": "MIT"
 }
